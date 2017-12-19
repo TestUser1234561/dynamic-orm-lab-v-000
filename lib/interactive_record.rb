b0c09830ac053d04524151f7f4ec12ad75b9cd3b
Morgan Fudge <mfudge92@gmail.com>
2017-12-19 17:03:02 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,7 +59,8 @@ class InteractiveRecord
 
     def self.find_by(args)
       col = args.keys.map do |val|
-        str = "#{val.to_s} = " + is_number?(args[val].to_s) ? args[val].to_s : "'#{args[val].to_s}'"
+        eq = is_number?(args[val].to_s) ? args[val].to_s : "'#{args[val].to_s}'"
+        str = "#{val.to_s} = " +
       end
       p col
       sql = "select * from #{self.table_name} where #{col}"
